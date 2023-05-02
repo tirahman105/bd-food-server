c5a28e58c142bc3f938138be493fc0f0f8d1326c
tirahman105 <tirahman105@gmail.com>
2023-05-02 17:37:35 +0600

    recipe data added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors =require('cors');
 const port = process.env.PORT || 5000;
 
 const allChef = require('./data/allChef.json')
+
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('BD food server is running')
