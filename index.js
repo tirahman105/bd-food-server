a191370cf47ff57475e678312675bb385e408415
tirahman105 <tirahman105@gmail.com>
2023-05-02 11:22:25 +0600

    data showed in local host

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allChef = require('./data/allChef.json')
+
 app.get('/', (req, res) =>{
     res.send('BD food server is running')
 });
+
+app.get('/allchef', (req, res)=>{
+    res.send(allChef);
+})
 
 app.listen(port, () =>{
     console.log(`BD API is running on port: ${port}`)
