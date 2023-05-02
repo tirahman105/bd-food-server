dbc52842a92374758a2d8bcf8a6a5ea39516ef08
tirahman105 <tirahman105@gmail.com>
2023-05-02 18:34:28 +0600

    data updated

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors =require('cors');
 const port = process.env.PORT || 5000;
 
-const allChef = require('./data/allChef.json')
+const allChef = require('./data/allChef.json');
+const recipe = require('./data/recipe.json');
 
 app.use(cors());
 
@@ -13,6 +14,16 @@ app.get('/', (req, res) =>{
 
 app.get('/allchef', (req, res)=>{
     res.send(allChef);
+});
+app.get('/recipe', (req, res)=>{
+    res.send(recipe);
+})
+
+app.get('/recipe/:id', (req, res)=> {
+    const id = req.params.id;
+    console.log(id);
+    const selectedRecipe = recipe.find(n => n._id === id);
+    res.send(selectedRecipe);
 })
 
 app.listen(port, () =>{
